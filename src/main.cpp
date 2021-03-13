e73ec0f3bf731acb013868af2a1330d368a4437c
ben-congram <ben.congram@robotics.utias.utoronto.ca>
2021-03-13 12:10:26 -0500

    Initial commit with serial dep

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "/tmp/usr/local/include/serial/serial.h"     // todo: shouldn't have to specify whole path
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
