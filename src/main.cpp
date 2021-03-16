c9c7c6cc0d91039aa2deba2b8dfbcdf69a8aed51
ben-congram <ben.congram@robotics.utias.utoronto.ca>
2021-03-16 11:36:35 -0400

    Add initial code to read byte from serial port

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,33 @@
+
 #include <iostream>
-#include "/tmp/usr/local/include/serial/serial.h"     // todo: shouldn't have to specify whole path
+#include <filesystem>
+
+#include "serial/serial.h"
+#include "rtklib.h"
+
+namespace fs = std::filesystem;
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+
+  std::string port0_path = "/dev/ttyUSB0";
+  if (!fs::exists(port0_path)) {
+    throw std::runtime_error("Serial connection not found. Check that USB is plugged in.");
+  }
+  unsigned long baud = 9600;
+
+  serial::Serial port0(port0_path, baud, serial::Timeout::simpleTimeout(1000));
+
+  unsigned char byte_in;
+
+  size_t total_bytes_read = 0;
+  while (true) {
+    size_t bytes_read = port0.read(&byte_in, 1);
+    total_bytes_read += bytes_read;
+
+    std::cout << bytes_read << ", byte read: " << byte_in << std::endl;
+
+    if (total_bytes_read > 100) break;      // temporary
+  }
+
   return 0;
 }
