d0711636c0bbb3adfb2105313f60a2a08d031fab
ben-congram <ben.congram@robotics.utias.utoronto.ca>
2021-04-06 17:40:16 -0400

    Add initial files for Python analysis package

---
+++ cpo_analysis/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'cpo_analysis'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ben',
+    maintainer_email='ben.congram@robotics.utias.utoronto.ca',
+    description='TODO: Package description',
+    license='BSD',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
