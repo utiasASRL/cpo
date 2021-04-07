0d62d7323256c264750e4c1cd1091378e8a699e1
ben-congram <ben.congram@robotics.utias.utoronto.ca>
2021-04-07 10:15:49 -0400

    Add estimates to live plot

--- cpo_analysis/cpo_analysis/plot_from_msg.py
+++ cpo_analysis/cpo_analysis/plot_from_msg.py
@@ -5,12 +5,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from cpo_interfaces.msg import TDCP
+from geometry_msgs.msg import PoseWithCovariance
 
 plt.ion()  # make plotting interactive
 
 # set up overhead plot
 fig, ax = plt.subplots()
-plot = ax.scatter([], [])
+plot = ax.scatter([], [], c='k')
+plot2 = ax.scatter([], [], c='g')
 ax.set_xlim(-5, 5)
 ax.set_ylim(-5, 5)
 
@@ -27,14 +29,20 @@ class TdcpSubscriber(Node):
       'tdcp',
       self.tdcp_callback,
       10)
-    self.tdcp_sub  # prevent unused variable warning
+    self.tdcp_sub
+    self.enu_est_sub = self.create_subscription(
+      PoseWithCovariance,
+      'cpo_enu',
+      self.enu_est_callback,
+      10)
+    self.enu_est_sub  # prevent unused variable warning
     # todo: add PoseWithCovariance subscriber
 
   def tdcp_callback(self, msg):
-    self.msg_count += 1
+    self.tdcp_msg_count += 1
     self.get_logger().info(
       'Found ENU position {0:.2f}, {1:.2f}, {2:.2f} [m] {3}'.format(msg.enu_pos.x, msg.enu_pos.y, msg.enu_pos.z,
-                                                                    self.msg_count))
+                                                                    self.tdcp_msg_count))
     point = (msg.enu_pos.x, msg.enu_pos.y)
 
     # plot East-North code solutions as we go
@@ -45,7 +53,21 @@ class TdcpSubscriber(Node):
     ax.set_ylim(array[:, 1].min() - 1, array[:, 1].max() + 1)
     fig.canvas.draw()
 
-  msg_count = 0
+  def enu_est_callback(self, msg):
+    self.enu_msg_count += 1
+    self.get_logger().info(
+      'Est. ENU position  {0:.2f}, {1:.2f}, {2:.2f} [m] {3}'.format(msg.pose.position.x, msg.pose.position.y,
+                                                                    msg.pose.position.z, self.enu_msg_count))
+    point = (msg.pose.position.x, msg.pose.position.y)
+
+    # plot East-North estimates as we go
+    array = plot2.get_offsets()
+    array = np.append(array, [point], axis=0)
+    plot2.set_offsets(array)
+    fig.canvas.draw()
+
+  tdcp_msg_count = 0
+  enu_msg_count = 0
 
 
 def main(args=None):
